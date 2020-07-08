e3fe0d384676aa08acb32a0e41da488af82f243b
PANUGANTI Srinivas <srinivas.panuganti@hexagonsi.com>
2020-07-08 23:50:07 +0530

    Added components

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import Hunts from './hunts';
+import Hunts from './components/hunts';
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_PH_GRAPHQL_URL
