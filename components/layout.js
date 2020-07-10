bd3d2b0a514245bd85b15ddcb96cd8f7201c0c4d
PANUGANTI Srinivas <srinivas.panuganti@hexagonsi.com>
2020-07-10 08:49:36 +0530

    Corrected case

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
-import Header from "./Header";
-import NavBar from "./NavBar";
+import Header from "./header";
+import NavBar from "./navBar";
 import 'semantic-ui-css/semantic.min.css';
 
 import "./Index.scss";
