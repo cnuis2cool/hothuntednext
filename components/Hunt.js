8741b619a24b2180685bd02457e00c6043d906b2
PANUGANTI Srinivas <srinivas.panuganti@hexagonsi.com>
2020-07-10 08:39:53 +0530

    Added folders back

---
+++ components/Hunt.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+    Button,
+    Card,
+    Grid,
+    Header,
+    Message,
+    Segment,
+} from 'semantic-ui-react';
+
+const Hunt = (props) => (
+    <div className="ui card" key={props.hunt.node.id} style={{ marginLeft: '1em' }}>
+        <div className="content">
+            <img src={props.hunt.node.thumbnail.url} className="ui mini right floated image" />
+            <div className="header">{props.hunt.node.name}</div>
+            <div className="meta">{props.hunt.node.tagline}</div>
+            <div className="description">
+                {props.hunt.node.description}
+            </div>
+        </div>
+        <div className="extra content">
+              <div>
+                <button className="ui teal right floated button">
+                    <a href={props.hunt.node.website} target="_blank" className="">Website</a>
+                </button>
+                <div className="ui left labeled button" role="button">
+                    {/* <button className="ui blue basic button">
+                        <i aria-hidden="true" className="sort up icon"></i>
+                            Votes
+                    </button>
+                    <a className="ui blue left pointing basic label">{props.hunt.node.votesCount}</a> */}
+                    <div class="ui right labeled button">
+                        <div class="ui icon button">
+                            <i>Votes</i>
+                        </div>
+                        <a class="ui basic label">{props.hunt.node.votesCount}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Hunt;
---
+++ pages/Index.js
@@ -0,0 +1,62 @@
+import Head from 'next/head';
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import Hunts from '../components/Hunts';
+
+import Layout from "../components/Layout";
+
+import {
+  Button,
+  Card,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+
+const client = new ApolloClient({
+  uri: 'https://api.producthunt.com/v2/api/graphql',
+  headers: {
+    authorization: `Bearer Y0qgFNy6xrPmmS6Gb5qN7EMWmaN7-O9tBrHqAkg6gBk` //${process.env.REACT_APP_PH_KEY}` // TODO: Env variable doesn't work
+  }
+});
+
+export default function Home() {
+  return (
+    <ApolloProvider client={client}>
+      <Layout>
+        <Hunts />
+      </Layout>
+      <footer className="footer">
+      <div className="ui inverted vertical segment">
+        <div className="ui container">
+          <div className="ui inverted stackable divided grid">
+            <div className="row">
+              <div className="computer only five wide column">
+                <h4 className="ui inverted header">Tech Stack</h4>
+                <div role="list" className="ui inverted horizontal list">
+                  <a role="listitem" className="item">ReactJS</a>
+                  <a role="listitem" className="item">Semantic UI</a>
+                  <a role="listitem" className="item">GraphQL</a>
+                  <a role="listitem" className="item">ApolloClient</a>
+                </div>
+              </div>
+              <div className="five wide column">
+                <h4 className="ui inverted header">Creator</h4>
+                <p>Made with 💚 by <a href="https://cnuis2cool.github.io/">Srinivas Panuganti</a></p>
+              </div>
+              <div className="computer tablet only five wide column">
+                <h4 className="ui inverted header">Credits</h4>
+                <p>Made possible by <a href="https://api.producthunt.com/v2/docs">ProductHunt API</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </ApolloProvider>
+  )
+}
